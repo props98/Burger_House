017426f2e8650d2e98ca74668482bad93b955175
MaxLocal <maxlocal@mail.ru>
2023-05-18 00:51:07 +0300

    Make changes / but not work

--- Burger_Angular/src/app/app.module.ts
+++ Burger_Angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
